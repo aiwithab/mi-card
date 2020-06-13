b833c3646b572135cb6c8000b902cee565e5ca30
aiwithab <abdulahadas@gmail.com>
2020-06-13 10:54:56 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# miCard 📇
+
+## What was my goal
+
+I wanted to make a contact card app and using the app in my pocket, I’ll be able share anyone my contact info.
+
+## What I created
+
+![Finished App]()
+
+## What I have learned
+
+- How to create Stateless Widgets
+- What is the difference between hot reload and hot refresh and running an app from cold
+- How to use Containers to lay out your UI
+- How to use Columns and Rows to position your UI elements
+- How to add custom fonts
+- How to add Material icons
+- How to style Text widgets
+- How to read and use Flutter Documentation
+
+>This is a companion project to The App Brewery's Complete Flutter Development Bootcamp.
---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+void main() {
+  runApp(MiCard());
+}
+
+class MiCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mi Card',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color(0xff444e71),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundColor: Color(0xffdbe2e8),
+              backgroundImage: AssetImage('assets/images/profile.jpeg'),
+            ),
+            Text(
+              'Abdul Ahad',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+                color: Color(0xffdbe2e8),
+                letterSpacing: 1.5,
+              ),
+            ),
+            Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+                fontFamily: 'Source Sans Pro',
+                color: Color(0xffdbe2e8),
+                letterSpacing: 1.5,
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+              width: 130.0,
+              child: Divider(
+                color: Color(0xffdbe2e8),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 20.0),
+              color: Color(0xffdbe2e8),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Color(0xff444e71),
+                ),
+                title: Text(
+                  '+91 8446 192253',
+                  style: TextStyle(
+                      color: Color(0xff444e71),
+                      fontFamily: 'Source Sans Pro',
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 20.0),
+              color: Color(0xffdbe2e8),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Color(0xff444e71),
+                ),
+                title: Text(
+                  'abdulahadas@live.com',
+                  style: TextStyle(
+                      color: Color(0xff444e71),
+                      fontFamily: 'Source Sans Pro',
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 20.0),
+              color: Color(0xffdbe2e8),
+              child: ListTile(
+                leading: Icon(
+                  Icons.favorite,
+                  color: Color(0xff444e71),
+                ),
+                title: Text(
+                  '@aiwithab',
+                  style: TextStyle(
+                      color: Color(0xff444e71),
+                      fontFamily: 'Source Sans Pro',
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
